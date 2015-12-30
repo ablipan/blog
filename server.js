20a8dabf9eb6e989aa8bcb1fb13b6f99eb9a9851
lipan <lipan3@asiainfo.com>
2015-12-30 23:47:57 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+/*
+* @Author: lipan
+* @Date:   2015-12-30 22:27:13
+* @Last Modified by:   lipan
+* @Last Modified time: 2015-12-30 22:32:02
+*/
+
+require('babel-core/register')
+require('./bin/www')
